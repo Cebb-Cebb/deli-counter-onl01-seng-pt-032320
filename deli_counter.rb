d52c2373711b379ad699218c494219fd6dafdab5
hersson ceballos <herssonceballos@gmail.com>
2020-04-05 22:10:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@ require 'pry'
 
 katz_deli = []
 
-def line(line)
-  if line == []
+def line(wait)
+  if wait == []
     puts "The line is currently empty."
   else
     sentence = "The line is currently:"
-    line.each_with_index do |name, index|
+    wait.each_with_index do |name, index|
       sentence += " #{index + 1}. #{name}"
     end
     puts sentence
