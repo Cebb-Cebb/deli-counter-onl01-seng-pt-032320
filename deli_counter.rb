76604434b2cc4988d25039294983001ac7666d11
hersson ceballos <herssonceballos@gmail.com>
2020-04-05 22:19:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,22 +5,22 @@ def line(wait)
     puts "The line is currently empty."
   else
     order = "The line is currently:"
-    wait.each_with_index do |name, index|
+    line.each_with_index do |name, index|
       order += " #{index + 1}. #{name}"
     end
     puts order
   end
 end
 
-def take_a_number(line, name)
-  puts "Welcome, #{name}. You are number #{line.size + 1} in line."
-  line.push(name)
-end
+# def take_a_number(line, name)
+#   puts "Welcome, #{name}. You are number #{line.size + 1} in line."
+#   line.push(name)
+# end
 
-def now_serving(line)
-  if line != []
-    puts "Currently serving #{line.shift}."
-  else
-    puts "There is nobody waiting to be served!"
-  end
-end
+# def now_serving(line)
+#   if line != []
+#     puts "Currently serving #{line.shift}."
+#   else
+#     puts "There is nobody waiting to be served!"
+#   end
+# end
