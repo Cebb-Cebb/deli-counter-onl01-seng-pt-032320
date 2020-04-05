4279d76d72892ea21f889105e5da42db04944f05
hersson ceballos <herssonceballos@gmail.com>
2020-04-05 21:55:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,27 +3,27 @@ require 'pry'
 
 katz_deli = []
 
-def line(queque)
-  if queque == []
+def line(line)
+  if line == []
     puts "The line is currently empty."
   else
-    phrase = "The line is currently:"
-    phrase.each_with_index do |name, index|
-      phrase += " #{index + 1}. #{name}"
+    sentence = "The line is currently:"
+    line.each_with_index do |name, index|
+      sentence += " #{index + 1}. #{name}"
     end
-    puts phrase
+    puts sentence
   end
 end
 
-# def take_a_number(line, name)
-#   puts "Welcome, #{name}. You are number #{line.size + 1} in line."
-#   line.push(name)
-# end
+def take_a_number(line, name)
+  puts "Welcome, #{name}. You are number #{line.size + 1} in line."
+  line.push(name)
+end
 
-# def now_serving(line)
-#   if line != []
-#     puts "Currently serving #{line.shift}."
-#   else
-#     puts "There is nobody waiting to be served!"
-#   end
-# end
+def now_serving(line)
+  if line != []
+    puts "Currently serving #{line.shift}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
