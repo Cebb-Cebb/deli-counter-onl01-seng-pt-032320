5b2989f7dc960df24651ac3ca8eaa71abafe9a9e
hersson ceballos <herssonceballos@gmail.com>
2020-04-05 21:33:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,17 +13,30 @@
 #         break
 #     end
 # end
+def line_simple(array) # this one follows the same mechanics as learn.co
+  current_line = "The simple line is currently:"
+  array.each.with_index(1) do |value, indexemus|  
+  # "each.with_index" is the method...must use "index"
+    current_line << " #{indexemus}. #{value},"    
+    # "indexemus" is used to illustrate this variable can be different from "index"
+  end 
+  puts current_line
+end 
 
-katz_deli = []
-def line(katz_deli)
-  phrase ="The line is currently: "
-  if katz_deli.length > 0 
-    katz_deli.each_with_index do |name,index|
-      phrase += "#{index+1}. #{name}"
-    end
-    puts phrase
-  else 
-    puts "The line is currently empty."
-  end
+
+
+
+
+# katz_deli = []
+# def line(katz_deli)
+#   phrase ="The line is currently: "
+#   if katz_deli.length > 0 
+#     katz_deli.each_with_index do |name,index|
+#       phrase += "#{index+1}. #{name}"
+#     end
+#     puts phrase
+#   else 
+#     puts "The line is currently empty."
+#   end
     
-end
+# end
