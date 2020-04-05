dadbfce0f676e759e187d73560b7df2b5cb30bd3
hersson ceballos <herssonceballos@gmail.com>
2020-04-05 21:48:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,29 @@
-katz_deli = []
-counter = 0
+# Write your code here.
+require 'pry'
 
-def line(katz_deli)
-  if katz_deli.length == 0
+katz_deli = []
+
+def line(line)
+  if line == []
     puts "The line is currently empty."
   else
-    line = "The line is currently:"
-    counter = 0
-    until counter == katz_deli.length
-      line << " #{counter+1}. #{katz_deli[counter]}"
-      counter += 1
+    sentence = "The line is currently:"
+    line.each_with_index do |name, index|
+      sentence += " #{index + 1}. #{name}"
     end
-    puts line;
+    puts sentence
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(line, name)
+  puts "Welcome, #{name}. You are number #{line.size + 1} in line."
+  line.push(name)
 end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0
-    puts "There is nobody waiting to be served!"
+def now_serving(line)
+  if line != []
+    puts "Currently serving #{line.shift}."
   else
-    puts "Currently serving #{katz_deli.shift}."
+    puts "There is nobody waiting to be served!"
   end
 end
