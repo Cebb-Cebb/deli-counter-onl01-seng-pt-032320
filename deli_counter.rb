e73ccf0f2cd98a29a65399c8f018b8c5714d0b43
hersson ceballos <herssonceballos@gmail.com>
2020-04-05 22:42:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(line, name)
 end
 
 def now_serving(line)
-  while line != []
+  if line != []
     puts "Currently serving #{line.shift}."
   else
     puts "There is nobody waiting to be served!"
